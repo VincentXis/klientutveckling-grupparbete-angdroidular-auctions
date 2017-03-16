6bfe73fa7558c22039bfba4b65c7b373852b6e64
Linus Blom <linus.blom@nackademin.se>
2017-03-16 13:43:06 +0100

    admin updates

--- html/app/admin/admin.controller.js
+++ html/app/admin/admin.controller.js
@@ -1,5 +1,13 @@
-angular.module("admin").controller("adminController", ["$scope", "$location", "loginService", function ($scope, $location, loginService) {
+angular.module("admin").controller("adminController", ["$scope", "$location", "loginService", "auctionService", function ($scope, $location, loginService, auctionService) {
+    $scope.completedAuctions = [];
+
     if(!loginService.isUserAdmin() || !loginService.isUserLoggedIn()) {
         $location.path("/");
     }
+
+    auctionService.getCompletedAuctions().then(function (response) {
+        $scope.completedAuctions = response.data;
+    });
+
+
 }]);
