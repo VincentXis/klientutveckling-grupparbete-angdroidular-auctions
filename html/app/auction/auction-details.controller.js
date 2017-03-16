0a7a22325849a3366235939e5c114765a8c246ae
Seyran <siran.vahabzadeh@nackademin.se>
2017-03-16 14:50:06 +0100

    added visa leverantör

--- html/app/auction/auction-details.controller.js
+++ html/app/auction/auction-details.controller.js
@@ -1,11 +1,15 @@
-angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams", "auctionService","bidService", function ($scope, $routeParams, auctionService, bidService) {
+angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams","$location", "auctionService","bidService", function ($scope, $routeParams,$location ,auctionService, bidService) {
     $scope.auction = {};
     $scope.bids= [];
     auctionService.getAuctionById($routeParams.id).then(function (response) {
         $scope.auction = response.data;
         bidService.getBids($routeParams.id).then(function(bidResponse){
             $scope.bids = bidResponse.data;
+            $scope.auction.bidPrice = bidResponse.data[bidResponse.data.length - 1].bidPrice;
         })
     });
+    showSupplier = function(){
+        $location.path("/supplier/"+ $scope.auction.supplierId )
+    }
 
 }]);
