813497dddda39760141d5e9f34615841f4c1d4a6
Seyran <siran.vahabzadeh@nackademin.se>
2017-03-16 13:47:09 +0100

    budhistorik added

--- html/app/auction/auction-details.controller.js
+++ html/app/auction/auction-details.controller.js
@@ -1,8 +1,11 @@
-angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams", "auctionService", function ($scope, $routeParams, auctionService) {
+angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams", "auctionService","bidService", function ($scope, $routeParams, auctionService, bidService) {
     $scope.auction = {};
-
+    $scope.bids= [];
     auctionService.getAuctionById($routeParams.id).then(function (response) {
         $scope.auction = response.data;
+        bidService.getBids($routeParams.id).then(function(bidResponse){
+            $scope.bids = bidResponse.data;
+        })
     });
 
 }]);
