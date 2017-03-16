224e08433d9ca5312ed60b0424eb14d23d3b51ff
Linus Blom <linus.blom@nackademin.se>
2017-03-16 12:34:11 +0100

    bidservice

--- html/app/app.module.js
+++ html/app/app.module.js
@@ -1,1 +1,1 @@
-angular.module("app", ["ui.bootstrap", "ngRoute", "ngAnimate", "main", "menu", "category", "login", "signup", "auction", "appfilter"]);
+angular.module("app", ["ui.bootstrap", "ngRoute", "ngAnimate", "main", "menu", "category", "login", "signup", "auction", "appfilter", "bid"]);
