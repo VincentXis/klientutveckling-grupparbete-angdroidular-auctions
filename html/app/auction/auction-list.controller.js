b8080a118e4515dd30983691cead6418f8c68512
VincentXis <rickard.bernal@gmail.com>
2017-03-17 15:43:04 +0100

    safety commit, scope auctions seems to have bids in it now.

--- html/app/auction/auction-list.controller.js
+++ html/app/auction/auction-list.controller.js
@@ -1,5 +1,6 @@
-angular.module("auction").controller("auctionListController", ["$scope", "$routeParams", "$location", "auctionService", function ($scope, $routeParams, $location, auctionService) {
+angular.module("auction").controller("auctionListController", ["$scope", "$routeParams", "$location", "$q", "auctionService", "bidService", function ($scope, $routeParams, $location, $q, auctionService, bidService) {
     var auctions = [];
+    var bids = [];
     $scope.limitByCategory = "";
     $scope.limitBySearch = "";
     $scope.auctionId = "";
@@ -8,17 +9,31 @@ angular.module("auction").controller("auctionListController", ["$scope", "$route
         auctions = response.data;
 
         $scope.limitByCategory = $routeParams.category;
-        $scope.limitBySearch= $routeParams.search;
+        $scope.limitBySearch = $routeParams.search;
+
 
         auctionService.getCompletedAuctions().then(function (response) {
+            var promises = [];
+
+            angular.forEach(auctions, function (auction) {
+                promises.push(bidService.getBids(auction.id));
+            });
+            $q.all(promises).then(function (response) {
+                for (var i = 0; i < auctions.length; i++) {
+                    auctions[i].bids = response[i].data;
+                }
+                console.log(auctions);
+
+                $scope.auctions = auctions;
+            });
+
             $scope.compAuctions = response.data;
-            $scope.auctions = auctions;
         });
 
 
     });
 
-    $scope.goToAuctionPage = function(id){
+    $scope.goToAuctionPage = function (id) {
         $location.path("/auction/" + id);
     }
 }]);
