bf5b7751a69b5a7602df45059bde5f5ff491d750
VincentXis <rickard.bernal@gmail.com>
2017-03-17 16:09:10 +0100

    decided not to show the current bid, added buyout price and description to the auctions view

--- html/app/auction/auction-list.controller.js
+++ html/app/auction/auction-list.controller.js
@@ -1,6 +1,5 @@
 angular.module("auction").controller("auctionListController", ["$scope", "$routeParams", "$location", "$q", "auctionService", "bidService", function ($scope, $routeParams, $location, $q, auctionService, bidService) {
     var auctions = [];
-    var bids = [];
     $scope.limitByCategory = "";
     $scope.limitBySearch = "";
     $scope.auctionId = "";
@@ -13,24 +12,9 @@ angular.module("auction").controller("auctionListController", ["$scope", "$route
 
 
         auctionService.getCompletedAuctions().then(function (response) {
-            var promises = [];
-
-            angular.forEach(auctions, function (auction) {
-                promises.push(bidService.getBids(auction.id));
-            });
-            $q.all(promises).then(function (response) {
-                for (var i = 0; i < auctions.length; i++) {
-                    auctions[i].bids = response[i].data;
-                }
-                console.log(auctions);
-
-                $scope.auctions = auctions;
-            });
-
+            $scope.auctions = auctions;
             $scope.compAuctions = response.data;
         });
-
-
     });
 
     $scope.goToAuctionPage = function (id) {
