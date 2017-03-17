047ec39feaec0e0149aa597729b85de36182cc15
DominicBoman <dominic_bohman@hotmail.com>
2017-03-17 10:30:18 +0100

    newbid changes

--- html/app/auction/auction-details.controller.js
+++ html/app/auction/auction-details.controller.js
@@ -1,15 +1,36 @@
-angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams","$location", "auctionService","bidService", function ($scope, $routeParams,$location ,auctionService, bidService) {
+angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams","$location", "auctionService","bidService", "loginService", function ($scope, $routeParams,$location ,auctionService, bidService, loginService) {
     $scope.auction = {};
     $scope.bids= [];
     auctionService.getAuctionById($routeParams.id).then(function (response) {
         $scope.auction = response.data;
         bidService.getBids($routeParams.id).then(function(bidResponse){
             $scope.bids = bidResponse.data;
-            $scope.auction.bidPrice = bidResponse.data[bidResponse.data.length - 1].bidPrice;
+
+                $scope.auction.bidPrice = bidResponse.data[bidResponse.data.length - 1].bidPrice;
+
+
         })
     });
     $scope.showSupplier = function(){
         $location.path("/supplier/"+ $scope.auction.supplierId )
+    }
+
+    $scope.bid = function(bidPrice){
+        if(!loginService.isUserLoggedIn()){
+            $location.path("/login/req");
+        }
+        else{
+            var userId = loginService.getLoggedInUser().id;
+            bidService.newBid($scope.auction.id, userId, bidPrice).then(function (response) {
+                $location.path("/thankyou");
+
+            }, function (response) {
+                console.log(response.data);
+            }
+
+            )
+        }
+
     }
 
 }]);
