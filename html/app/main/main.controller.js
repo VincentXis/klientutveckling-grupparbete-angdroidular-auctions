a5361c088011ad5e4799d139f9d331d334442541
Linus Blom <linus.blom@nackademin.se>
2017-03-20 15:31:54 +0100

    download link

--- html/app/main/main.controller.js
+++ html/app/main/main.controller.js
@@ -1,4 +1,4 @@
-angular.module("main").controller("mainController", ["$scope", function ($scope) {
-
+angular.module("main").controller("mainController", ["$scope", "$http", function ($scope, $http) {
+    $scope.fileHref = "http://localhost:3000/test.apk";
 }]);
 
